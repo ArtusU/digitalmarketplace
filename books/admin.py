e85ef4a23a0048ac897527a97508dac1f08445bb
ArtusU <artusrock@hotmail.com>
2020-09-22 17:21:56 +0100

    UserLibrary model

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Author, Book, Chapter, Exercise, Solution
+from .models import Author, Book, Chapter, Exercise, Solution, UserLibrary
 
 
 admin.site.register(Author)
@@ -7,5 +7,7 @@ admin.site.register(Book)
 admin.site.register(Chapter)
 admin.site.register(Exercise)
 admin.site.register(Solution)
+admin.site.register(UserLibrary)
+
 
 
